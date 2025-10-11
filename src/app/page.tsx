2edab6950b203a59f4ac64cf0d7136c91e73e5aa
Patolord <rodrigo_luizp@hotmail.com>
2025-10-10 23:19:55 -0300

    Add GSAP library and update package dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const messages = useQuery(api.functions.getForAuthenticatedUser);
-
+  
   return (
-    <div>
-      {messages?.map((message) => (
-        <div key={message._id}>{message.body} - {message.user}</div>
-      ))}
+    <div className="bg-black text-white flex justify-center items-center h-screen">
+     Hello
     </div>
   );
 }
