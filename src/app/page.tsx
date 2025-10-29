7f3ec640ece53817e2cda347f5e87e847f82d84d
patolord <rodrigo_luizp@hotmail.com>
2025-10-28 21:43:33 -0300

    Update configuration and styles; change Tailwind base color to 'neutral', add Cloudinary domain for images in Next.js config, and update package dependencies to include '@calcom/embed-react'. Refactor globals.css for consistent font usage and improve shadow properties. Enhance homepage and schedule page with GSAP animations and calendar integration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
 export default function Home() {
+  const box = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    gsap.to(box.current, {
+      x: 100,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  });
   return (
     <div>
       <h1>Home</h1>
+      <div ref={box} className="w-10 h-10 bg-red-500"></div>
     </div>
   );
 }
--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+
 export default function Calendar() {
+  useEffect(() => {
+    (async () => {
+      const cal = await getCalApi({});
+      cal("ui", {
+        styles: { branding: { brandColor: "#000000" } },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
+  }, []);
+
   return (
-    <div>
-      <h1>Calendar</h1>
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold mb-8">Agende sua Sessão</h1>
+      <Cal
+        calLink="upcraft-crew/training-session"
+        style={{ width: "100%", height: "100%", overflow: "scroll" }}
+        config={{ layout: "month_view" }}
+      />
     </div>
   );
 }
